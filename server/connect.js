5f87fd67fc9b0f9b5205562144e45a77d8f1e6e3
Krishnangshu Sinha <128132191+krishnangshusinha@users.noreply.github.com>
2024-02-18 00:01:12 +0530

    Update connect.js

--- server/connect.js
+++ server/connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://krishnangshusinha15:JIyWm0TP886ZW0s4@registrationform.n31g42n.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://krishnangshusinha15:JIyWm0TP886ZW0s4@registrationform.n31g42n.mongodb.net/?retryWrites=true&w=majority/employee")
 .then(()=> console.log("Connection Successfull"))
 .catch(()=> console.log("Error in connection"))
